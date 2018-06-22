260bd1187bf61ac89eb177de3da534bceda6e2ad
LucienXian <lucienxian97@gmail.com>
2018-06-23 00:56:54 +0800

    add wordlist

--- learn/urls.py
+++ learn/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('wordbook/', views.wordbook, name='wordbook'),
     path('wordbook/wordbookunit', views.wordbookunit, name='wordbookunit'),
+    path('wordbook/wordbookunit/wordlist', views.wordlist, name='wordlist'),
 ]
--- learn/views.py
+++ learn/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import WordBook, WordUnit
+from .models import WordBook, WordUnit, WordInUnit
 # Create your views here.
 
 def get_all_books():
@@ -52,6 +52,7 @@ def wordbookunit(request):
                 info = {}
                 info['unit_name'] = 'unit' + str(wb.unit_order)
                 info['word_num'] = wb.word_num
+                info['unit_id'] = wb.id
                 all_words_num += wb.word_num
                 wb_info.append(info)
             passed['allunits'] = wb_info
@@ -59,4 +60,29 @@ def wordbookunit(request):
             passed['wordbook_id'] = wordbook_id
     return render(request, 'wordbookunit.html', passed)
 
+# @login_required
+def wordlist(request):
+    passed = {}
+    passed['wordbook_info'] = get_all_books()
+    if request.method == "GET":
+        if 'id' in request.GET:
+            unit_id = request.GET['id']
+            get_unit = WordUnit.objects.get(id=unit_id)
+            get_book = get_unit.book
+            passed['title'] = get_book.title
+            passed['unit'] = 'unit' + str(get_unit.unit_order)
+            passed['word_num'] = get_unit.word_num
+            passed['uploaded_user'] = get_book.uploaded_user.username
+            passed['username'] = get_book.uploaded_user.username
+            passed['wordbook_id'] = get_book.id
+            all_words = WordInUnit.objects.filter(wordunit=get_unit)
+            
+            words_list = []
+            for wordunit in all_words:
+                word_info = {}
+                word_info['spelling'] = wordunit.word.spelling
+                word_info['mean'] = wordunit.word.meaning
+                words_list.append(word_info)
+            passed['all_words'] = words_list
+    return render(request, 'wordlist.html', passed)
 
